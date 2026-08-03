20d31fac3fa32d2a17270cf6905514e98052c70c
Manas Ranjan Bera <manasbera484@gmail.com>
2026-08-03 19:39:32 +0530

    JS

---
+++ JS/Class 1/script.js
@@ -0,0 +1,33 @@
+// console.log("Hello");
+
+
+//! Variable Keywords
+//? var
+// var a = 10
+// console.log(a)
+// var a = 20
+// console.log(a)
+// a = "Hello"
+// console.log(a)
+
+//? let
+// let b = 10
+// console.log(b)
+// // let b = 30 //not allowed
+// b = 20
+// // let b //not allowed
+// console.log(b)
+
+//? const
+const c = 1
+// const c = 20
+// c = 10 //not allowed
+
+
+let x
+x = 10
+
+var y
+y = 20
+
+const z = 10
