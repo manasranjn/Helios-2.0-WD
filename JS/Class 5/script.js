b8a31210b7383fdcac43f26723f77ef007dc5f4d
Manas Ranjan Bera <manasbera484@gmail.com>
2026-08-07 19:23:32 +0530

    Array methods

---
+++ JS/Class 5/script.js
@@ -0,0 +1,61 @@
+//! Array Inbuilt Methods
+let arr = [10, 20, 'Manas', true, 40, 60]
+
+//? push()
+// arr.push(1, 2, 3, 4, 5)
+// console.log(arr);
+
+//? pop()
+// arr.pop()
+// console.log(arr);
+
+//? unshift()
+// arr.unshift(1, 2, 3, 4, 5)
+// console.log(arr);
+
+//? shift()
+// arr.shift()
+// console.log(arr);
+
+//? slice()
+let subArr = arr.slice(1, 4)
+// console.log(subArr);
+
+//? splice()
+// arr.splice(2, 0, 100, 200, 300)
+// console.log(arr);
+// arr.splice(1, 3)
+// console.log(arr);
+// arr.splice(2, 1, false, undefined)
+// console.log(arr);
+
+//? reverse()
+// arr.reverse()
+// console.log(arr);
+
+//? length
+// console.log(arr.length);
+
+//? includes()
+// console.log(arr.includes(1));
+// console.log(arr.includes(10));
+
+//? forEach()
+// arr.forEach((item) => {
+//     console.log(item);
+// })
+
+let price = [100, 300, 250, 650, 590, 520]
+// price.forEach((p) => {
+//     console.log("Original Price- ", p);
+
+//     console.log("Discounted Price", p - p * 0.10);
+// })
+
+//? map()
+
+//? filter()
+
+//? reduce()
+
+//? sort()
